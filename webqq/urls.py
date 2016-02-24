488ca3499d87e173486e6e8e01be9641d82fa862
zhou in window <13276915582@163.com>
2016-02-24 19:41:00 +0800

    新增用户接收信息url

--- webqq/urls.py
+++ webqq/urls.py
@@ -4,4 +4,5 @@ import views
 urlpatterns = [
     url(r'^$', views.dashboard, name="chat"),
     url(r'^send_msg/$', views.send_msg, name="chat_send_msg"),
+    url(r'^get_msg/$', views.get_msg, name="get_new_msg"),
 ]
